3649762eea888f662b8882f76be3343133826df8
MARKOTHEDEV <ogechuwkumatthew@gmail.com>
2021-09-03 06:00:56 -0600

    named the urls in the blogapp

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     # this represent the home page url
     # path('',views.IndexPageListView.as_view())
-    path('',views.index),
+    path('',views.index,name='home'),
     path('post/<int:pk>/',views.PostDetail.as_view(),name='post-detail')
 
 ]
